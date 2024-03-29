8a3120fbc6c2b39aeb5642db99210e327a68e26f
vu <nhieucon2016ncvn@gmail.com>
2024-03-29 23:56:38 +0700

    ban goc

--- GiuaKyBE2NhomK/app/Models/User.php
+++ GiuaKyBE2NhomK/app/Models/User.php
@@ -21,24 +21,17 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'phone',
+        
     ];
-
-    /**
+	
+	/**
      * The attributes that should be hidden for serialization.
      *
      * @var array<int, string>
      */
     protected $hidden = [
-        'password',
-        'remember_token',
+        'password'
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
 }
