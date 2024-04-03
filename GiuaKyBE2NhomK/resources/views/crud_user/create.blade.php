a8b47626b7e62e8a915334745f4bb13cf09a284a
quynhnhu11 <quynhnhu20020911@gmail.com>
2024-04-03 11:49:27 +0700

    update function login 03/04/2024

---
+++ GiuaKyBE2NhomK/resources/views/crud_user/create.blade.php
@@ -0,0 +1,58 @@
+@extends('dashboard')
+
+@section('content')
+    <main class="signup-form">
+        <div class="cotainer">
+            <div class="row justify-content-center">
+                <div class="col-md-4">
+                    <div class="card">
+                        <h3 class="card-header text-center">Create User</h3>
+                        <div class="card-body">
+                            <form action="{{ route('user.createUser') }}" method="POST" enctype="multipart/form-data">
+                                @csrf
+                                <div class="form-group mb-3">
+                                    <input type="text" placeholder="Name" id="name" class="form-control" name="name"
+                                           required autofocus>
+                                    @if ($errors->has('name'))
+                                        <span class="text-danger">{{ $errors->first('name') }}</span>
+                                    @endif
+                                </div>
+                                <div class="form-group mb-3">
+                                    <input type="text" placeholder="Email" id="email_address" class="form-control"
+                                           name="email" required autofocus>
+                                    @if ($errors->has('email'))
+                                        <span class="text-danger">{{ $errors->first('email') }}</span>
+                                    @endif
+                                </div>
+                                <div class="form-group mb-3">
+                                    <input type="password" placeholder="Password" id="password" class="form-control"
+                                           name="password" required>
+                                    @if ($errors->has('password'))
+                                        <span class="text-danger">{{ $errors->first('password') }}</span>
+                                    @endif
+                                </div>
+                                <div class="form-group mb-3">
+                                    <input type="number" placeholder="Phone" id="phone" class="form-control"
+                                           name="phone" required autofocus>
+                                    @if ($errors->has('phone'))
+                                        <span class="text-danger">{{ $errors->first('phone') }}</span>
+                                    @endif
+                                </div>
+                                <div class="form-group mb-3">
+                                    <input type="file" placeholder="fileToUpload" id="fileToUpload" class="form-control"
+                                           name="image" required>
+                                    @if ($errors->has('phone_image'))
+                                        <span class="text-danger">{{ $errors->first('image') }}</span>
+                                    @endif
+                                </div>
+                                <div class="d-grid mx-auto">
+                                    <button type="submit" class="btn btn-dark btn-block">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+@endsection
---
+++ GiuaKyBE2NhomK/resources/views/crud_user/list.blade.php
@@ -0,0 +1,38 @@
+@extends('dashboard')
+
+@section('content')
+    <main class="login-form">
+        <div class="container">
+            <div class="row justify-content-center">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Phone</th>
+                            <th>Image</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($users as $user)
+                            <tr>
+                                <th>{{ $user->id }}</th>
+                                <th>{{ $user->name }}</th>
+                                <th>{{ $user->email }}</th>
+                                <th>{{ $user->phone }}</th>
+                                <td><img style="width: 70px; height: 70px" src="{{ asset('uploads/userimage/' . $user->image) }}" alt="Phone Image"></td>
+                                <th>
+                                    <a href="">View</a> |
+                                    <a href="{{ route('user.UpdatetUser', ['id' => $user->id]) }}">Edit</a> |
+                                    <a href="{{route('user.deleteUser',['id' => $user->id]) }}">Delete</a>
+                                </th>
+                            </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </main>
+@endsection
